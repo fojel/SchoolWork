332d47fc6a9d3b4f46e243da29007763aefa9b0e
Fojel <79102693+fojelio@users.noreply.github.com>
2023-07-06 20:22:52 -0300

    Update and rename JavaScript 29 - Class_01.js to  Class01.js

--- JavaScript/ Class01.js
+++ JavaScript/ Class01.js
@@ -1,28 +1,22 @@
 /*
-    CONSIGNA: Escribir un formulario en html y un script en JS para imprimir el area del 
-              rectangulo creando una clase llamada 'CRectangulo' con una funcion  llamada
-              'CalcularArea' que devuelve el area.
-
-              La longitud y la altura se pasan como parametros a su constructor.
+Escribir un formulario en html y un script en JS para imprimir el area del 
+rectangulo creando una clase llamada 'CRectangulo' con una funcion  llamada 'CalcularArea' que devuelve el area.
+La longitud y la altura se pasan como parametros a su constructor.
 */
 
-class CRectangulo
-{
-    constructor(A, B)
-    {
+class CRectangulo{
+    constructor(A, B){
         this.A = A;
         this.B = B;
     }
 
-    CalcularArea()
-    {
+    CalcularArea(){
         return (this.A*2) + (this.B*2);
     }
 }
 
 const formulario = document.getElementById('formulario');
-formulario.addEventListener('submit', function(event) 
-{
+formulario.addEventListener('submit', function(event){
   event.preventDefault();
 
   const A = document.getElementById('longitud').value;
