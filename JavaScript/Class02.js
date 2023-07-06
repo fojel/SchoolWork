de9af1e424417089c98d76f813002e0824eb110a
Fojel <79102693+fojelio@users.noreply.github.com>
2023-07-06 20:30:59 -0300

    Update Class02.js

--- JavaScript/Class02.js
+++ JavaScript/Class02.js
@@ -1,39 +1,30 @@
 /*
-    CONSIGNA: Escriba un programa que imprima la informacion (nombre, anio de ingreso, salario, 
-              direccion) creando una clase llamada 'CEmpleado'. La salida debe ser la siguiente:
+Escriba un programa que imprima la información (nombre, año de ingreso, salario, dirección)   creando una clase llamada 'CEmpleado'. La salida debe ser la siguiente:
+Name        Year of joining        Address
 
-              Name        Year of joining        Address
-
-              Robert      2014                   64C- WallStreet
-
-
-              Imprimir los anios de antiguedad.
-              Imprimir si el empleado vive en la calle Wall Street.
+Robert        2014                       64C- WallStreet
+Imprimir los años de antigüedad
+Imprimir si el empleado vive en la calle Wall Street
 */
 
-class CEmpleado
-{
-    constructor(nombre, anio_ingreso, salario, direccion)
-    {
+class CEmpleado{
+    constructor(nombre, anio_ingreso, salario, direccion){
         this.nombre = nombre;
         this.anio_ingreso = anio_ingreso;
         this.salario = salario;
         this.direccion = direccion;
     }
 
-    mostrarDatos()
-    {
+    mostrarDatos(){
         return "Nombre: " + this.nombre + "<br> Anio de ingreso: " + this.anio_ingreso 
                 + "<br> Salario: " + this.salario + "<br> Direccion: " + this.direccion;
     }
 
-    calcularAntiguedad()
-    {
+    calcularAntiguedad(){
         return 2022 - this.anio_ingreso;
     }
 
-    viveWallStreet()
-    {
+    viveWallStreet(){
         return this.direccion == 'Wall Street';
     }
 }
