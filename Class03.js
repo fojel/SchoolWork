4861e092de8f24856c104c89a3934a3c509898a1
Fojel <79102693+fojelio@users.noreply.github.com>
2023-07-06 20:25:41 -0300

    Update and rename JavaScript 31 - Class_03.js to Class03.js

--- Class03.js
+++ Class03.js
@@ -1,27 +1,15 @@
 /*
-    CONSIGNA: Hacer un formulario html para que se pueda ingresar la CPersona que contemple 
-              las siguientes condiciones:
-
-              -Sus atributos son: Nombre, FechaNacimiento, DNI, Genero, peso y altura.
-              -Por defecto, todos los atributos menos el DNI seran valores por defecto segun su tipo
-              (0 numeros, cadena vacia para String, etc.).
-              -Se implantara el constructor:
-              -Los metodos que se implementaran son:
-              -calcularIMC(): calculara si la persona esta en su peso ideal 
-              (peso en kg/(altura^2 en m)), si esta formula devuelve un valor menor que 20, la 
-              funcion devuelve un -1, si devuelve un numero entre 20 y 25 (incluidos), significa que 
-              esta por debajo de su peso ideal la funcion devuelve un 0 y si devuelve un valor mayor 
-              que 25 significa que tiene sobrepeso, la funcion devuelve un 1.
-              -esMayorDeEdad(): indica si es mayor de edad, devuelve un booleano- Se contrasta con 
-              hora-dia del sistema. 
-              -ComprobarGenero(genero): comprueba que el genero introducido es correcto. Si no es 
-              correcto, sera H. Sera visible al exterior.
+   Hacer una clase llamada CPassword que siga las siguientes condiciones:
+    Que tenga los atributos longitud y contraseña . Por defecto, la longitud será de 8
+     Los métodos que implementa serán:  
+    esFuerte(): 
+    devuelve un booleano si es fuerte o no, para que sea fuerte debe tener mas de 2 mayúsculas, mas de 1 minúscula y mas de 5 números.
+    o generarPassword(): 
+    genera la contraseña con caracteres aleatorios con la longitud de 8 (se debe contemplar  mayúsculas, mas de 1 minúscula y mas de 5 números).
 */
 
-class CPersona
-{
-    constructor(nombre, fecha_nac, dni, genero, peso, altura, edad)
-    {
+class CPersona{
+    constructor(nombre, fecha_nac, dni, genero, peso, altura, edad){
         this.nombre = nombre;
         this.fecha_nac = fecha_nac;
         this.dni = dni;
@@ -31,8 +19,7 @@ class CPersona
         this.edad = edad;
     }
 
-    calcularIMC()
-    {
+    calcularIMC(){
         if(this.peso / (this.altura * this.altura) < 20)
             return -1;
         else if(this.peso / (this.altura * this.altura) >= 20 && this.peso / (this.altura * this.altura) <= 25)
@@ -41,21 +28,18 @@ class CPersona
             return 1;
     }
 
-    esMayorDeEdad()
-    {
+    esMayorDeEdad(){
         return this.edad > 17;
     }
     
-    comprobarGenero()
-    {
+    comprobarGenero(){
         if(this.genero == 'F' || this.genero == 'M' || this.genero == 'X')
             return "El genero es correcto.";
         else
             return "El genero es incorrecto.";
     }
 
-    calcularIMC2(peso)
-    {
+    calcularIMC2(peso){
         if(peso == -1)
             return "Esta por debajo de su peso ideal";
         else if(peso == 0)
@@ -66,8 +50,7 @@ class CPersona
 }
 
 const formulario = document.getElementById('formulario');
-formulario.addEventListener('submit', function(event) 
-{
+formulario.addEventListener('submit', function(event){
   event.preventDefault();
 
   const nombre = document.getElementById('nombre').value;
