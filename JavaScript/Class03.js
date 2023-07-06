c22bab852955b39a912c1b976160b606ec393987
Fojel <79102693+fojelio@users.noreply.github.com>
2023-07-06 20:28:41 -0300

    Update Class03.js

--- JavaScript/Class03.js
+++ JavaScript/Class03.js
@@ -1,11 +1,12 @@
 /*
-   Hacer una clase llamada CPassword que siga las siguientes condiciones:
-    Que tenga los atributos longitud y contraseña . Por defecto, la longitud será de 8
-     Los métodos que implementa serán:  
-    esFuerte(): 
-    devuelve un booleano si es fuerte o no, para que sea fuerte debe tener mas de 2 mayúsculas, mas de 1 minúscula y mas de 5 números.
-    o generarPassword(): 
-    genera la contraseña con caracteres aleatorios con la longitud de 8 (se debe contemplar  mayúsculas, mas de 1 minúscula y mas de 5 números).
+Hacer un formulario html para que se pueda ingresar la CPersona que contemple las siguientes condiciones:
+Sus atributos son: Nombre, FechaNacimiento, DNI, Genero  ( tener en cuenta Decreto 476/2021 - https://www.lavoz.com.ar/ciudadanos/identidades-no-binarias-a-un-ano-del-decreto-una-persona-por-dia-solicita-el-cambio-de-dni/ ) , peso y altura.
+Por defecto, todos los atributos menos el DNI serán valores por defecto según su tipo (0 números, cadena vacía para String, etc.).
+Se implantara el constructor:
+Los métodos que se implementaran son:
+calcularIMC(): calculara si la persona esta en su peso ideal (peso en kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, la función devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), significa que esta por debajo de su peso ideal la función devuelve un 0 y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la función devuelve un 1.
+esMayorDeEdad(): indica si es mayor de edad, devuelve un booleano- Se contrasta con hora-día del sistema. 
+ComprobarGenero(genero): comprueba que el genero introducido es correcto. Si no es correcto, sera H. Sera visible al exterior.
 */
 
 class CPersona{
